dcca93d8e63ee67de725fff2a975246f542f63b6
Rahly Haikal <muhammadrahly01@gmail.com>
2026-08-04 21:30:30 +0700

    Tambah App.Config dan Koneksi Datbase

---
+++ WinFormsApp1/WinFormsApp1/DataAccess/DBConnection.cs
@@ -0,0 +1,19 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Text;
+
+namespace WinFormsApp1.DataAccess
+{
+    public class DBConnection
+    {
+        private static string connString =
+            ConfigurationManager.ConnectionStrings["AnomalyVersusDB"].ConnectionString;
+
+        public static MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(connString);
+        }
+    }
+}
